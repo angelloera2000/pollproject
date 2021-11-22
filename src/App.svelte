c5f32ad4f94bc9015b3d5ca97914b116af69b655
angelloera2000 <angelloera.vivendo@gmail.com>
2021-11-22 15:28:30 -0600

    Actualizacion 22-11-21
    Se cambio toda la interfaz, agrega, elimina y edita registros sin problema

--- src/App.svelte
+++ src/App.svelte
@@ -1,97 +1,190 @@
 <script>
-    
-    let allData = [];
-    let data = { name: "", idCard: "", email: "", phone: "" };
+  import { v4 } from "uuid";
+  import Noty from "noty";
 
-    if (localStorage.getItem("frm")) {
-        allData = JSON.parse(localStorage.getItem("frm"));
+  import "noty/lib/noty.css";
+
+
+
+  let products = [];
+
+  let product = {
+    id: "",
+    name: "",
+    description: "",
+    category: "",
+    imageURL: ""
+  };
+
+  if (localStorage.getItem("frm")) {
+    products = JSON.parse(localStorage.getItem("frm"));
     }
+    
+   $: localStorage.setItem("frm", JSON.stringify(products))
 
-    let editStatus = false;
+  let editStatus = false;
 
-    const cleanProduct = () => {
-        data = {
-            name: "",
-            idCard: "",
-            email: "",
-            phone: "",
-        };
+  const cleanProduct = () => {
+    product = {
+      id: "",
+      name: "",
+      description: "",
+      category: "",
+      imageURL: ""
+    };
+  };
+
+  const addProduct = () => {
+    const newProduct = {
+      id: v4(),
+      name: product.name,
+      description: product.description,
+      category: product.category,
+      imageURL: product.imageURL
     };
 
-    const saveData = () => {
-        const newData = {
-            name: data.name,
-            idCard: data.idCard,
-            email: data.email,
-            phone: data.phone,
-        };
+    products = products.concat(newProduct);
 
-        allData = allData.concat(newData);
+    cleanProduct();
+    localStorage.setItem("frm", JSON.stringify(products));
+  };
 
-        cleanProduct();
-        console.log(allData);
-        localStorage.setItem("frm", JSON.stringify(allData));
+  const deleteProduct = id => {
+    products = products.filter(item => item.id !== id);
+  };
+
+  const editProduct = productEdited => {
+    editStatus = true;
+    product = productEdited;
+  };
+
+  const updateProduct = () => {
+    let updatedProduct = {
+      name: product.name,
+      description: product.description,
+      id: product.id,
+      imageURL: product.imageURL,
+      category: product.category
     };
+    const productIndex = products.findIndex(p => p.id === product.id);
+    products[productIndex] = updatedProduct;
+    cleanProduct();
+    new Noty({
+      theme: "sunset",
+      type: "success",
+      timeout: 3000,
+      text: "Product Update Successfully"
+    }).show();
+    editStatus = false;
+  };
 
-    const editData = (dataEdited) => {
-        editStatus = true;
-        data = dataEdited;
-        console.log(data);
-    };
-
-    //Se esta trabajando en esto por el momento
-    const updateData = () => {
-        let updateData = {
-            name: data.name,
-            idCard: data.idCard,
-            email: data.email,
-            phone: data.phone
-        };
-        const productIndex = allData.findIndex(p => p.id === data.id);
-        allData[productIndex] = updateData;
-        cleanProduct();
-        editStatus = false;
-    };
-
-    const deleteData = (id) => {
-        console.log(id);
-        allData = allData.filter((data) => data.idCard !== id);
-        localStorage.removeItem("frm");
-    };
-
-    const onSubmitHandler = () => {
-            saveData();
-    };
+  const onSubmitHandler = () => {
+    if (!editStatus) {
+      addProduct();
+    } else {
+      updateProduct();
+    }
+  };
 </script>
 
-<main>
-    <form on:submit|preventDefault={onSubmitHandler}>
-        <h2 class="card-title">
-            {#if !editStatus}Add Data{:else}Update Data{/if}
-          </h2>
-        <div style="float:left; width:200px;">
-            <input placeholder="Name" bind:value={data.name} /> <br />
-            <input placeholder="Id Card" bind:value={data.idCard} /> <br />
-            <input placeholder="Email" bind:value={data.email} /> <br />
-            <input placeholder="Phone" bind:value={data.phone} /> <br />
-            <button type="submit">
-                {#if !editStatus}Save Product{:else}Update Product{/if}
-            </button>
-        </div>
-    </form>
-    {#each allData as data}
-        <table id="table">
-            <tr>
-                <td>{data.name}</td>
-                <td>{data.idCard}</td>
-                <td>{data.email}</td>
-                <td>{data.phone}</td>
-                <button on:click={deleteData(data.idCard)}> Delete </button>
-                <!-- <button on:click={editData(data.idCard)}> Edit </button> -->
-            </tr>
-        </table>
-    {/each}
-</main>
-
 <style>
+
 </style>
+
+<main>
+  <div class="container p-4">
+    <div class="row">
+      <div class="col-md-6">
+        {#each products as product}
+          <div class="card mt-2 animate__animated animate__backInLeft">
+					<div class="row">
+						<div class="col-md-4">
+						{#if !product.imageURL}
+						<img src="images/no-product.png" class="p-2 img-fluid" alt="Image"/>
+						{:else}
+						<img src="{product.imageURL}" class="p-2 img-fluid" alt="Image"/>
+						{/if}
+						</div>
+						<div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">
+                <strong>{product.name}</strong>
+                <span>
+                  <small>{product.category}</small>
+                </span>
+              </h5>
+              <p class="card-text">{product.description}</p>
+              <button
+                class="btn btn-danger"
+                on:click={deleteProduct(product.id)}>
+                Delete
+              </button>
+              <button class="btn btn-secondary" on:click={editProduct(product)}>
+                Edit
+              </button>
+            </div>
+						</div>
+					</div>
+          </div>
+        {/each}
+      </div>
+      <div class="col-md-6">
+        <div class="card">
+          <div class="card-body">
+            <h2 class="card-title">
+              {#if !editStatus}Add Product{:else}Update Product{/if}
+            </h2>
+            <form on:submit|preventDefault={onSubmitHandler}>
+              <div class="form-group">
+                <label for="product-name">Product Name</label>
+                <input
+                  bind:value={product.name}
+                  type="text"
+                  class="form-control"
+                  id="product-name"
+                  placeholder="Product Name" />
+              </div>
+
+              <div class="form-group">
+                <label for="product-description">Product Description</label>
+                <textarea
+                  bind:value={product.description}
+                  id="product-description"
+                  rows="3"
+                  class="form-control"
+                  placeholder="Product Description" />
+              </div>
+
+              <div class="form-group">
+                <label for="produtc-image-url">Product Image URL</label>
+                <input
+                  bind:value={product.imageURL}
+                  type="url"
+                  id="product-image-url"
+                  class="form-control"
+                  placeholder="https://faztweb.com" />
+              </div>
+
+              <div class="form-group">
+                <label for="category">Product Category</label>
+                <select
+                  id="category"
+                  class="form-control"
+                  bind:value={product.category}>
+                  <option selected disabled>Select a Category</option>
+                  <option value="laptops">Laptops</option>
+                  <option value="peripherals">Peripherals</option>
+                  <option value="servers">Servers</option>
+                </select>
+              </div>
+              <button type="submit" class="btn btn-primary">
+                {#if !editStatus}Save Product{:else}Update Product{/if}
+              </button>
+            </form>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
